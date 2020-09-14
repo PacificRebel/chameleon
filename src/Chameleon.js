1a6829b3e34dc0777eb6ffcfc2210f26c9a0e85a
Heli Sivunen <heli@mooncat.tv>
2020-09-14 18:07:28 +0100

    Rollback index html to test Heroku crash

--- src/Chameleon.js
+++ src/Chameleon.js
@@ -21,18 +21,12 @@ changeToYellow = () => {
 }
 
   render() {
-    const buttonStyle = {
-      fontFamily: "Quattrocento",
-      fontSize: 40
-    }
     return(
       <div className="change-colour">
-      <h1 style={buttonStyle}>
         <button onClick={this.changeToRed}>Change to Red</button>
         <button onClick={this.changeToBlue}>Change to Blue</button>
         <button onClick={this.changeToYellow}>Change to Yellow</button>
           <Colour newColour={this.state.currentColour}/>
-          </h1>
       </div>
     )
   }

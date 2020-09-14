b6905673ceb5f2d795d2c2151f6cc630fa80b596
Heli Sivunen <heli@mooncat.tv>
2020-09-14 18:46:14 +0100

    Add Procfile and other changes for Heroku deploy

---
+++ build/precache-manifest.c52c8e52395fde72be2e393097f3135f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40572fed27f6e7ddcd660eb2ff9be08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87e5b6a1a68b12212951",
+    "url": "/static/css/main.eab05bfb.chunk.css"
+  },
+  {
+    "revision": "f1c80d8950ea6afa81a2",
+    "url": "/static/js/2.ecb34f96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ecb34f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e5b6a1a68b12212951",
+    "url": "/static/js/main.6623e416.chunk.js"
+  },
+  {
+    "revision": "049ef2111a2c1074fe2f",
+    "url": "/static/js/runtime-main.10743df1.js"
+  }
+]);
